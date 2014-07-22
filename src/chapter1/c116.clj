2ae8619ef1906d2257c5f344e2d90d75fd4e9933
Milan Aleksic <milanaleksic@gmail.com>
2014-07-22 16:38:21 +0200

    c118.clj added

--- src/chapter1/c116.clj
+++ src/chapter1/c116.clj
@@ -8,6 +8,20 @@
         (= n 0) 0
         (< n 0) (- n)))
 
+(defn abs2
+  [n]
+  (cond (< n 0) (- n)
+        :else n))
+
+(defn abs3
+  [n]
+  (if (< n 0) (- n)
+        n))
+
+(defn gte [x y]
+  (or (> x y) (= x y)))
+
 (defn -main
   [& args]
-  (println (str "Absolute of -5 is: " (abs (- 5)))))
+  (println (str "10 >= 5 == " (gte (10 5))))
+  (println (str "Absolute of -5 is: " (abs3 (- 5)))))
---
+++ src/chapter1/c118.clj
@@ -0,0 +1,28 @@
+(ns chapter1.c118
+  (:require [clojure.java.io :as io])
+  (:gen-class :main true))
+
+(defn abs [n]
+  (if (< n 0) (- n)
+    n))
+
+(defn square [x] 
+  (* x x))
+
+(defn average [x y]
+  (/ (+ x y) 2))
+
+(defn sqrt [x]
+  (defn good-enough? [guess]
+    (< (abs (- (square guess) x)) 0.001))
+  (defn improve [guess]
+    (average guess (/ x guess)))
+  (defn sqrt-iter [guess]
+    (if (good-enough? guess)
+        guess
+        (sqrt-iter (improve guess))))
+  (sqrt-iter 1.0))
+
+(defn -main [& args]
+  (println "hello world!")
+  (println (str "Square root of 81 is: " (sqrt 81))))
